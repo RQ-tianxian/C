eb04f3c72e9f99af359504c2698e424c8031587b
RQ-tianxian <2139595324@qq.com>
2021-01-23 20:32:12 +0800

    2021_01_23

---
+++ test_01_23/test_01_23/demo.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));//(2)
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));//(4)
+}
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//(1)
+	printf("%d\n", sizeof(ch));//(3)
+	test1(arr);
+	test2(ch);
+	return 0;
+}
